3c3a7ea17efd281225bbdb9de4e15aee7fdd9280
boatlay <3596449821@qq.com>
2024-10-08 17:40:44 +0800

    Add files via upload

---
+++ Java_structure/src/com/ISEKAI/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.ISEKAI;
+
+import com.ISEKAI.tool.Print;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        Print.printMessage("This is a test message from Print class");
+    }
+}
---
+++ Java_structure/src/com/ISEKAI/tool/Print.java
@@ -0,0 +1,7 @@
+package com.ISEKAI.tool;
+
+public class Print {
+    public static void printMessage(String message) {
+        System.out.println(message);
+    }
+}
